183a9c75fb91a7dcea412831fb29e92b1b5e3cac
etienne.de-longeaux <etienne.de-longeaux@alterway.fr>
2016-02-14 15:59:53 +0100

    add bundle files

---
+++ SfynxDatabaseBundle.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of the <Database> project.
+ *
+ * @category   Database
+ * @package    Bunlde
+ * @author     Etienne de Longeaux <etienne.delongeaux@gmail.com>
+ * @copyright  2015 PI-GROUPE
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    2.3
+ * @link       http://opensource.org/licenses/gpl-license.php
+ * @since      2015-02-16
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Sfynx\DatabaseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Sfynx configuration and managment of the Database Bundle
+ *
+ * @category   Database
+ * @package    Bunlde
+ * @author     Etienne de Longeaux <etienne.delongeaux@gmail.com>
+ * @copyright  2015 PI-GROUPE
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    2.3
+ * @link       http://opensource.org/licenses/gpl-license.php
+ * @since      2015-02-16
+ */
+class SfynxDatabaseBundle extends Bundle
+{
+    /**
+     * Builds the bundle.
+     *
+     * It is only ever called once when the cache is empty.
+     *
+     * This method can be overridden to register compilation passes,
+     * other extensions, ...
+     *
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     * 
+     * @author <etienne de Longeaux> <etienne.delongeaux@gmail.com>
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+    
+    /**
+     * Boots the Bundle.
+     */
+    public function boot()
+    {
+    }
+    
+    /**
+     * Shutdowns the Bundle.
+     */
+    public function shutdown()
+    {
+    }
+}
